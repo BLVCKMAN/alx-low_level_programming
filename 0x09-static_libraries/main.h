00c766feb73f3ecf2a4fe34ca81e935c7612b918
BLVCKMAN <oloyedeoa23@gmail.com>
2022-07-26 22:24:20 -0700

    damn

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,5 +17,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-#endif
+char *_strstr(char *haystack, char *needle)
