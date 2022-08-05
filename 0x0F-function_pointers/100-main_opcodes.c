32768572ec2ba20209cd4469d14045b27c0e0d7d
BLVCKMAN <oloyedeoa23@gmail.com>
2022-08-05 04:14:18 -0700

    ender

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,41 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
+ * main - prints its own opcodes
+ * @argc: number of arguments
  * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-	unsigned char *func_ptr;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
 	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
